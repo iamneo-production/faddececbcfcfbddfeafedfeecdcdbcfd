4450840dac50a5a67efaab37c05c5b911019a113
Iamneo <techadmin@iamneo.ai>
2023-09-30 07:57:37 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,47 +1,57 @@
 import React, { useState } from 'react';
-import './App.css';
-
-function App() {
+import './'
+const App = () => {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
 
   const addTask = () => {
     if (newTask.trim() !== '') {
-      setTasks([...tasks, newTask]);
+      setTasks([...tasks, { text: newTask, completed: false }]);
       setNewTask('');
     }
   };
 
-  const removeTask = (index) => {
+  const completeTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index].completed = true;
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (index) => {
     const updatedTasks = [...tasks];
     updatedTasks.splice(index, 1);
     setTasks(updatedTasks);
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>To-Do List</h1>
-        <div>
-          <input
-            type="text"
-            value={newTask}
-            onChange={(e) => setNewTask(e.target.value)}
-            placeholder="Enter a new task"
-          />
-          <button onClick={addTask}>Add Task</button>
-        </div>
-        <ul>
-          {tasks.map((task, index) => (
-            <li key={index}>
-              {task}
-              <button onClick={() => removeTask(index)}>Remove</button>
-            </li>
-          ))}
-        </ul>
-      </header>
+    <div>
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        placeholder="Add a new task"
+        value={newTask}
+        onChange={(e) => setNewTask(e.target.value)}
+        data-testid="task-input"
+      />
+      <button onClick={addTask} data-testid="add-button">
+        Add
+      </button>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index} className={task.completed ? 'completed' : ''}>
+            {task.text}
+            <button onClick={() => completeTask(index)} data-testid={`complete-button-${index}`}>
+              Complete
+            </button>
+            <button onClick={() => deleteTask(index)} data-testid={`delete-button-${index}`}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
